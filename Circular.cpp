1859e06753a959ad4fa4b8c5df52655d9a449069
Faizaditya-oss <faiz.aditya.ft24@mail.umy.ac.id>
2025-06-12 18:47:15 +0700

    case 3

--- Circular.cpp
+++ Circular.cpp
@@ -154,7 +154,11 @@ int main()
                     q.remove();
                     break;
                 }
-                
+                case '3':
+                {
+                    q.display();
+                    break;
+                }
             }
         }
       }
