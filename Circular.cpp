072922d050f90b16ad70c7c864734fda16cbc391
Faizaditya-oss <faiz.aditya.ft24@mail.umy.ac.id>
2025-06-24 15:31:08 +0700

    memperbaiki code

--- Circular.cpp
+++ Circular.cpp
@@ -1,23 +1,70 @@
+/**
+ * @mainpage Documentation Circular Queues
+ * 
+ * @section Introduction
+ * Project ini merupakan project struktur data
+ * menggunakan struktur data queues dengan pendekatan circular arrays.
+ * 
+ * @section Operations
+ * Project ini memiliki beberapa operasi antara lain:
+ * 1.Insert
+ * 2.Delete 
+ * 3.Display
+ * 
+ * @section Cara penggunaan
+ * Berikut beberapa menu yang bisa digunakan:
+ * 1. en queue
+ * 2. de queue
+ * 3. display
+ * 4. exit
+ * 
+ * @author Profile
+ * -nama : Faiz Aditya 
+ * -nim  : 20240140065 
+ * -kelas: B 
+ * 
+ * @brief
+ * @version 1.0
+ * @date 2025-06-24
+ * 
+ * @copyright { Faiz Aditya }@umy.ac.id (c) 2025
+ */
+
 #include <iostream>
 using namespace std;
-
+/**
+ * @class Queues
+ * @brief class ini untuk operasi lengkap queues
+ * 
+ * 
+ */
 class Queues
 {
-    private:
-      static const int max =5;
-      int FRONT, REAR;
-      int queue_array[5];
+private:
+    int FRONT; //<variable private front untuk menyimpan posisi depan antrian
+    int REAR; //<variable private rear untuk menyimpan posisi belakang antrian
+    int max = 5; //<variable private max untuk menyimpan ukuran maximum antrian
+    int queue_array[5]; //<variable private queue_array untuk menyimpan elemen antrian
 
-    public : 
-      Queues()
-      {
+public:
+/**
+ * @brief Construct a new Queues object
+ * set default queues null
+ * with front = -1 and rear = -1
+ */
+    Queues()
+    {
         FRONT = -1;
         REAR = -1;
-      }
+    }
 
-      void insert()
-      {
-        int num;
+    /**
+     * @brief method untuk memasukkan data dalam antrian
+     * data dimasukkan dalam variable queue_array
+     */
+    void insert()
+    {
+        int num; //< variable num untuk menyimpan nilai
         cout << "Enter a number: ";
         cin >> num;
         cout << endl;
@@ -26,7 +73,7 @@ class Queues
         if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1))
         {
             cout << "\nQueue overflow\n"; // 1.a
-            return;                      // 1.b
+            return;                       // 1.b
         }
 
         // 2. Cek apakah antrian kosong
@@ -35,28 +82,32 @@ class Queues
             FRONT == 0; // 2.a
             REAR = 0;   // 2.b
         }
-        else 
+        else
         {
             // jika REAR berada di posisi terakhir array, kembali ke awal array
-            if (REAR == max -1)
-            REAR = 0;
+            if (REAR == max - 1)
+                REAR = 0;
             else
-              REAR = REAR + 1;
+                REAR = REAR + 1;
         }
 
         queue_array[REAR] = num;
-      }
+    }
 
-      void remove()
-      {
+    /**
+     * @brief method untuk menghapus data dalam antrian
+     * data dihapuskan dari dalam variable queue_array
+     */
+
+    void remove()
+    {
         // Cek apakah antrian kosong
         if (FRONT == -1)
         {
             cout << "Queue underflow\n";
             return;
         }
-        cout << "\nThe element deleted from the queue is: " << queue_array
-        [FRONT] << "\n";
+        cout << "\nThe element deleted from the queue is: " << queue_array[FRONT] << "\n";
 
         // Cek jika antrian hanya memiliki satu elemen
         if (FRONT == REAR)
@@ -67,72 +118,78 @@ class Queues
         else
         {
             // Jika elemen yang dihapus berada di posisi terakhir array, kembali ke awal array
-            if (FRONT == max -1)
+            if (FRONT == max - 1)
                 FRONT = 0;
-            else  
-               FRONT = FRONT + 1;
-            
+            else
+                FRONT = FRONT + 1;
         }
-      }
-
-        void display()
-        {
-            int FRONT_position = FRONT;
-            int REAR_position = REAR;
-
-            // Cek apakah antrian kosong
-            if (FRONT == -1)
-            {
-                cout << "Queue is empty\n";
-                return;
-            }
-
-            cout << "\nElements in the queue are...\n";
-
-            //  Jika FRONT_position <= REAR_position, literasi dari FRONT hingga REAR
-            if (FRONT == -1)
-            {
-                while (FRONT_position <= REAR_position)
-                {
-                    cout << queue_array[FRONT_position] << "  ";
-                    FRONT_position++;
-                }
-                cout << endl;
-            }
-            else 
-            {
-                // Jika  FRONT_position > REAR_position, literasi dari FRONT higga akhir array
-                while (FRONT_position <= max -1)
-                {
-                    cout << queue_array[FRONT_position] << " ";
-                    FRONT_position++;
-                }
-                
-
-                FRONT_position = 0;
-
-
-                // Literasi dari awal array hingga ke REAR
-                while (FRONT_position <= REAR_position)
-                {
-                    cout << queue_array[FRONT_position] << " ";
-                    FRONT_position++;
-                }
-                cout << endl;
-            }
-
-        }
-        
     }
 
+    /**
+     * @brief method untuk menampilkan data dalam antrian
+     * data ditampilkan yang berada dalam variable queue_array
+     */
+
+    void display()
+    {
+        int FRONT_position = FRONT; //< variable front_position untuk menandakan posisi elemen
+        int REAR_position = REAR; //< variable rear_position untuk menandakan posisi elemen
+
+        // Cek apakah antrian kosong
+        if (FRONT == -1)
+        {
+            cout << "Queue is empty\n";
+            return;
+        }
+
+        cout << "\nElements in the queue are...\n";
+
+        //  Jika FRONT_position <= REAR_position, literasi dari FRONT hingga REAR
+        if (FRONT == -1)
+        {
+            while (FRONT_position <= REAR_position)
+            {
+                cout << queue_array[FRONT_position] << "  ";
+                FRONT_position++;
+            }
+            cout << endl;
+        }
+        else
+        {
+            // Jika  FRONT_position > REAR_position, literasi dari FRONT higga akhir array
+            while (FRONT_position <= max - 1)
+            {
+                cout << queue_array[FRONT_position] << " ";
+                FRONT_position++;
+            }
+
+            FRONT_position = 0;
+
+            // Literasi dari awal array hingga ke REAR
+            while (FRONT_position <= REAR_position)
+            {
+                cout << queue_array[FRONT_position] << " ";
+                FRONT_position++;
+            }
+            cout << endl;
+        }
+    }
+
+};
+
+/**
+ * @brief method utama untuk menjalankan program
+ * 
+ * @return int
+ */
 
 int main()
-    {
-      Queues q;
-      char ch;
+{
+    Queues q; //< objek untuk menggunakan sumber yang ada pada kelas queues
+    char ch; //< variable ch untuk menyimpan pilihan pada menu yang diberikan
 
-      while (true)
-      {
+    while (true)
+    {
         try
         {
             cout << "Menu" << endl;
@@ -146,38 +203,38 @@ int main()
 
             switch (ch)
             {
-                case '1':
-                {
+            case '1':
+            {
 
-                    q.insert();
-                    break;
-                }
-                case '2':
-                {
-                    q.remove();
-                    break;
-                }
-                case '3':
-                {
-                    q.display();
-                    break;
-                }
-                case '4':
-                {
-                    return 0;
-                }
-                default :
-                {
-                    cout << "Invalid  option!" << endl;
-                    break;
-                }
+                q.insert();
+                break;
             }
+            case '2':
+            {
+                q.remove();
+                break;
+            }
+            case '3':
+            {
+                q.display();
+                break;
+            }
+            case '4':
+            {
+                return 0;
+            }
+            default:
+            {
+                cout << "Invalid  option!" << endl;
+                break;
+            }
+            }
+        }
             catch (exception &e)
             {
                 cout << "Check for the values entered." << endl;
             }
-        
-      }
     }
-};
-
+    
+    return 0;
+}
